0e5b5b09cca38ea2a1468b3d559ebf50bdc3ca72
HaeRheeNam <4ran0318@gmail.com>
2024-02-09 01:33:13 +0900

    [Feat#32] 헤더, 이미지 업로드, 버튼 구성

--- src/components/styles/MypageStyle.jsx
+++ src/components/styles/MypageStyle.jsx
@@ -2,22 +2,17 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const MyPageSection = styled.section`
-    min-width: 320px;
+    min-width: 400px;
+    width: 1060px;
     max-width: 1060px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    border: 1px solid white;
 `;
 
 export const MyPageheadDiv = styled.div`
-    background-color: var(--mainColor);
+    background-color: var(--subColor3);
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    width: 100%;
 `;
 
 export const HomeLink = styled(Link)`
@@ -37,3 +32,34 @@ export const HeaderLogo = styled.img`
     width: 253px;
     height: 56px;
 `;
+
+export const ImageNdInfo = styled.article`
+    background-color: var(--mainColor);
+    width: 1060px;
+
+    padding: 44px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    gap: 30px;
+`;
+export const ProfileThumbnailImg = styled.img`
+    max-width: 230px;
+    max-height: 230px;
+
+    object-fit: cover;
+`;
+export const ProfileUpLoadLabel = styled.label`
+    max-width: 225px;
+    max-height: 58px;
+
+    border: 1px solid var(--subColor2);
+`;
+export const ProfileUpLoad = styled.input`
+    display: none;
+    background-color: red;
+`;
+
+export const UserImage = styled.div``;
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Layout from "components/layout/Layout";
 import defaultImage from "assets/defaultImage.png";
 import logoImage from "assets/logoImage.png";
 import * as S from "components/styles/MypageStyle";
-import Footer from "components/Footer/Footer";
 const dummyData = {
     userId: crypto.randomUUID(),
     nickname: "보라돌이",
@@ -12,21 +11,45 @@ const dummyData = {
 };
 
 const MyPage = () => {
+    const [imgUpFile, setImgUpFile] = useState("");
+    const imgRef = useRef();
+    const addImgFile = () => {
+        const imgFile = imgRef.current.files[0];
+        const reader = new FileReader();
+    };
     const data = dummyData;
     return (
         <Layout>
             <S.MyPageSection>
                 <S.MyPageheadDiv>
-                    <div>
-                        <div>
-                            <S.HomeLink to="/">← HOME</S.HomeLink>
-                            <h3>
-                                <S.HeaderLogo src={logoImage} alt="logo" />
-                            </h3>
-                            <h4>마이페이지</h4>
-                        </div>
-                    </div>
+                    <S.HomeLink to="/">← HOME</S.HomeLink>
+                    <h3>
+                        <S.HeaderLogo src={logoImage} alt="logo" />
+                    </h3>
+                    <h4>마이페이지</h4>
                 </S.MyPageheadDiv>
+                <S.ImageNdInfo>
+                    <S.UserImage>
+                        <S.ProfileThumbnailImg src={imgUpFile ? imgUpFile : defaultImage} alt="" />
+                        <S.ProfileUpLoadLabel htmlFor="ImgfileChoice">
+                            이미지 업로드
+                        </S.ProfileUpLoadLabel>
+                        <S.ProfileUpLoad
+                            type="file"
+                            accept="image/*"
+                            id="ImgfileChoice"
+                            ref={imgRef}
+                            onChange={addImgFile}
+                        />
+                    </S.UserImage>
+                    <div>
+                        <p>보라돌이</p>
+                        <p>나는 재즈가 좋아</p>
+                    </div>
+                    <ul>
+                        <button>편집</button>
+                    </ul>
+                </S.ImageNdInfo>
             </S.MyPageSection>
         </Layout>
     );
