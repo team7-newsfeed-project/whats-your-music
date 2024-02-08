52500f6b17884bf3e64183e1feb6365d83768a09
baram55 <asd7973@gmail.com>
2024-02-08 21:44:37 +0900

    [feat/#24]firebase 환경 변수 키 설정 및 키 감춤

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import GlobalStyle from "GlobalStyle";
 import Layout from "components/layout/Layout";
 import Router from "shared/Router";
+import { app, auth } from "database/firebase";
 
 function App() {
+    console.log("app : ", app);
+    //console.log("info : ", auth.currentUser);
     return (
         <>
             <GlobalStyle />
