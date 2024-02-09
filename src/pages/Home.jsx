ccbb7076332673ba0af7437ee72595b64d2eb9e3
Hyeryeon-Seo <0168968060@naver.com>
2024-02-09 15:06:49 +0900

    [update] main.jsx 이름 수정으로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Main from "components/main/Main";
+import Main from "components/main/RecommendListMain";
 import Layout from "components/layout/Layout";
-import Header from "components/main/Header";
+import Header from "components/Header/Header";
+import RecommendListMain from "components/main/RecommendListMain";
 
 const Home = () => {
     return (
         <Layout>
             <Header />
-            <Main />
+            <RecommendListMain />
         </Layout>
     );
 };
