d383201bbe6e69d83794e18c6058549fe83e0d0f
baram55 <asd7973@gmail.com>
2024-02-09 22:13:27 +0900

    [feat/#37]파이어베이스 API 구글 로그인 기능 구현

--- src/store/config/configStore.js
+++ src/store/config/configStore.js
@@ -2,9 +2,11 @@ import { createStore } from "redux";
 import { combineReducers } from "redux";
 import userId from "store/modules/userId";
 import category from "store/modules/category";
+import userAccount from "store/modules/userAccount";
 
 const rootReducer = combineReducers({
     userId,
+    userAccount,
     category,
 });
 const store = createStore(rootReducer);
