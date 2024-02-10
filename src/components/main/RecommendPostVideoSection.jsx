1b1c932b84f8e1617cc38f65575a3c635abd85d9
Hyeryeon-Seo <0168968060@naver.com>
2024-02-10 21:55:45 +0900

    [add] 홈 메인페이지 videosection컴포넌트 추가

---
+++ src/components/main/RecommendPostVideoSection.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecommendPostVideoSection = () => {
+    return <VideoSection>RecommendPostVideoSection</VideoSection>;
+};
+
+export default RecommendPostVideoSection;
