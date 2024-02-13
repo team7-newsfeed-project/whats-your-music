2595f972214f1626e2c0b5da808e749576ed83f3
Hyeryeon-Seo <0168968060@naver.com>
2024-02-13 15:29:25 +0900

    [feat/#95] 메인페이지 리스트 date순 정렬 (sort)

--- src/components/main/RecommendListMain.jsx
+++ src/components/main/RecommendListMain.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import MainPostItem from "./RecommendPostItem";
-import { dummyData } from "shared/postsDummyData";
 import { useDispatch, useSelector } from "react-redux";
 import { db } from "database/firebase";
 import { collection, getDocs, query } from "firebase/firestore";
@@ -9,7 +8,6 @@ import { setPost } from "store/modules/posts";
 
 const RecommendListMain = () => {
     // 홈 페이지의 메인 영역
-    // const [posts, setPosts] = useState([]);
     const setPosts = useSelector((state) => state.posts);
     const activeCategory = useSelector((state) => state.category);
     const dispatch = useDispatch();
@@ -19,19 +17,23 @@ const RecommendListMain = () => {
             // firestore db 가져오기
             const q = query(collection(db, "posts"));
             const querySnapshot = await getDocs(q);
-            // console.log(querySnapshot);
 
             const initialPosts = [];
 
             querySnapshot.forEach((doc) => {
                 initialPosts.push({ id: doc.id, ...doc.data() });
             });
+
+            // date 날짜시간순 정렬
+            const dateOrderedPosts = [...initialPosts].sort((a, b) => {
+                return new Date(b.date) - new Date(a.date);
+            });
+
             // firestore에서 가져온 데이터를 redux 통해 전달
-            dispatch(setPost(initialPosts));
+            dispatch(setPost(dateOrderedPosts));
         };
         fetchData();
     }, []);
-    // console.log(setPosts);
     const filteredPosts = setPosts.filter((post) => post.category === activeCategory);
 
     return (
@@ -65,7 +67,6 @@ const MainWrapper = styled.main`
     /* width: 1650px; */
     width: 100%;
     min-height: 700px;
-    /* border-radius: 20px; */
     margin-bottom: 10px;
 `;
 
@@ -75,19 +76,14 @@ const MainBanner = styled.div`
     width: 100%;
     height: 200px;
     background-image: url("https://images.unsplash.com/photo-1630441099796-851705faa3ed?q=80&w=1909&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-    /* background-size: 50%; */
     background-size: cover;
     background-repeat: no-repeat;
     background-position: right 35% bottom 30%;
-    /* background-color: #b192d1; */
-    /* background-color: var(--subColor1); */
     display: flex;
     justify-content: center;
     align-items: center;
-    /* color: black; */
     color: white;
     font-family: "Pretendard-Regular";
-    /* border-radius: 30px; */
     font-weight: bold;
     letter-spacing: 0.4rem;
 `;
@@ -98,14 +94,10 @@ const MainPostList = styled.ul`
     flex-wrap: wrap;
     gap: 20px;
     margin: 0px 30px 100px 30px;
-    /* background-color: bisque; */
 `;
 
 const ListText = styled.p`
     color: gray;
     font-size: 13px;
-    /* margin-bottom: 0px; */
-    /* padding-bottom: 50px; */
-    /* background-color: aqua; */
     height: 20px;
 `;
