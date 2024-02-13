dfb16983c6507ec669030c869985619978c8522d
HaeRheeNam <4ran0318@gmail.com>
2024-02-14 02:57:40 +0900

    [Chore] 마이페이지에 폰트 적용

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -20,6 +20,7 @@ time, mark, audio, video {
 	font-size: 100%;
 	font: inherit;
 	vertical-align: baseline;
+    font-family: "Pretendard-Regular"
 }
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure, 
